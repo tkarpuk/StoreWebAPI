a4039e793ab577cf46d8ce1114a599b2fe5c1604
tkarpuk <87716589+tkarpuk@users.noreply.github.com>
2021-08-31 10:47:52 +0300

    Initial: add test controller.

---
+++ StoreWebAPI/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        /// <summary>
+        /// Return current time for testing application
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public string GetCurrentTime()
+        {
+            return DateTime.Now.ToString("T");
+        }
+
+    }
+}
