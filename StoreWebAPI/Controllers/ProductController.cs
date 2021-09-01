455e2aa7f3905573146bd321b5b75f25b1951652
tkarpuk <87716589+tkarpuk@users.noreply.github.com>
2021-09-01 16:13:20 +0300

    1. Rename GetProductByIdQuerey 2. Implemented ProductController.

--- StoreWebAPI/Controllers/ProductController.cs
+++ StoreWebAPI/Controllers/ProductController.cs
@@ -1,10 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using MediatR;
+using StoreWebAPI.Features.ProductFeatures.Queries;
+using StoreWebAPI.Models.DB;
+using StoreWebAPI.Features.ProductFeatures.Commands;
 
 namespace StoreWebAPI.Controllers
 {
@@ -16,6 +15,41 @@ namespace StoreWebAPI.Controllers
         public ProductController(IMediator mediator)
         {
             this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var list = await mediator.Send(new GetProductsQuery());
+            return Ok(list);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProductById(int Id)
+        {
+            var item = await mediator.Send(new GetProductByIdQuery(Id));
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(Product product)
+        {
+            await mediator.Send(new AddProductCommand(product));
+            return StatusCode(201);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProductById(int Id)
+        {
+            await mediator.Send(new DeleteProductCommand(Id));
+            return StatusCode(201);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateStore(Product product)
+        {
+            await mediator.Send(new UpdateProductCommand(product));
+            return StatusCode(201);
         }
     }
 }
--- StoreWebAPI/Features/ProductFeatures/Queries/GetProductByIdQuery.cs
+++ StoreWebAPI/Features/ProductFeatures/Queries/GetProductByIdQuery.cs
@@ -7,18 +7,18 @@ using StoreWebAPI.Models.DB;
 
 namespace StoreWebAPI.Features.ProductFeatures.Queries
 {
-    public record GetProductById(int Id) : IRequest<Product> { }
+    public record GetProductByIdQuery(int Id) : IRequest<Product> { }
 
-    public class GetProductByIdHandler : IRequestHandler<GetProductById, Product>
+    public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, Product>
     {
         private readonly IRepository<Product> repository;
 
-        public GetProductByIdHandler(StoreDB storeDB)
+        public GetProductByIdQueryHandler(StoreDB storeDB)
         {
             repository = new RepositoryBase<Product>(storeDB);
         }
 
-        public async Task<Product> Handle(GetProductById request, CancellationToken cancellationToken)
+        public async Task<Product> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
         {
             return await Task.Run(() => repository.GetById(request.Id), cancellationToken);
         }
